1db73acdca4ab616ce218ca1f6b04e165976a477
aspenhi <david.yamato@gmail.com>
2025-01-17 07:40:18 -0700

    parent appreciation

---
+++ location of blog files.R
@@ -0,0 +1,11 @@
+getwd()
+list.files()
+
+setwd("/Users/davidkang/Library/CloudStorage/OneDrive-moxtek.com/R/20240908 Blog2024/posts")
+list.files()
+
+setwd("/Users/davidkang/Library/CloudStorage/OneDrive-moxtek.com/R/")
+list.files()
+
+setwd("/Users/davidkang/Library/CloudStorage/OneDrive-moxtek.com/R/20240214 Family History/posts")
+list.files()
